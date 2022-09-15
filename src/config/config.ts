7aaaccc2a64e39020084b3aa70f6ce82361c7a08
ihab khammouma <ihab.khammouma@gmail.com>
2022-09-15 11:32:08 +0100

    change url api marvel to https

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 const NODE_ENV = "development";
-const API_ENDPOINT = "http://gateway.marvel.com/v1/public/";
+const API_ENDPOINT = "https://gateway.marvel.com/v1/public/";
 const PUBLIC_KEY = "5f052f2e3d23c5a82d95c53e3111eaca";
 const PRIVATE_KEY = "b2d05db8e90f96a000d51f8f15dd7a62951fc218";
 const BACKEND_API = "http://127.0.0.1:5000";
